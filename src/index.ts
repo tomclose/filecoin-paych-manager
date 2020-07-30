7b7fe0368b5fce171f3df6ab067929dc8a9a43ba
Tom Close <tom.close@cantab.net>
2020-07-30 13:22:44 +0100

    Install filecoin packages and very basic interaction with lotus node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello wrld!');
+import { LotusRPC } from '@filecoin-shipyard/lotus-client-rpc';
+import { NodejsProvider } from '@filecoin-shipyard/lotus-client-provider-nodejs';
+import { testnet } from '@filecoin-shipyard/lotus-client-schema';
+
+const url = 'ws://127.0.0.1:7777/rpc/v0';
+// const url = 'http://127.0.0.1:7777/rpc/v0'
+// const url = 'wss://lotus.testground.ipfs.team/api/0/node/rpc/v0'
+// const provider = new BrowserProvider(url)
+// const provider = new NodejsProvider(url, { transport: 'http' })
+const provider = new NodejsProvider(url);
+const client = new LotusRPC(provider, { schema: testnet.fullNode });
+
+async function run() {
+  try {
+    const version = await client.version();
+    console.log('Version', version);
+  } catch (e) {
+    console.error('client.version error', e);
+  }
+  await client.destroy();
+}
+run();
---
+++ typings/@filecoin-shipyard/index.d.ts
@@ -0,0 +1,3 @@
+declare module '@filecoin-shipyard/lotus-client-rpc';
+declare module '@filecoin-shipyard/lotus-client-provider-nodejs';
+declare module '@filecoin-shipyard/lotus-client-schema';
